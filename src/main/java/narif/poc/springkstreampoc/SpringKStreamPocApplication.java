a2663c74e81e3a8066352ce8dfd9337bb7e7aae9
Najeeb Arif <Najeeb_Arif@gap.com>
2022-05-17 03:03:28 +0530

    Update SpringKStreamPocApplication.java

--- src/main/java/narif/poc/springkstreampoc/SpringKStreamPocApplication.java
+++ src/main/java/narif/poc/springkstreampoc/SpringKStreamPocApplication.java
@@ -4,16 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import narif.poc.springkstreampoc.model.OrderInputMsg;
 import narif.poc.springkstreampoc.model.Tuple;
 import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.StreamRetryTemplate;
 import org.springframework.context.annotation.Bean;
-import org.springframework.retry.RetryPolicy;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 
 import java.util.Map;
 import java.util.function.Function;
@@ -24,21 +19,6 @@ public class SpringKStreamPocApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKStreamPocApplication.class, args);
-    }
-
-    @Bean
-    @StreamRetryTemplate
-    RetryTemplate myRetryTemplate() {
-        RetryTemplate retryTemplate = new RetryTemplate();
-
-        RetryPolicy retryPolicy = new SimpleRetryPolicy(3);
-        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
-        backOffPolicy.setBackOffPeriod(2);
-
-        retryTemplate.setBackOffPolicy(backOffPolicy);
-        retryTemplate.setRetryPolicy(retryPolicy);
-
-        return retryTemplate;
     }
 
     @Bean
@@ -71,43 +51,6 @@ public class SpringKStreamPocApplication {
             final Tuple<Throwable, OrderInputMsg> inputMsgTuple = new Tuple<>(e, value);
             return new KeyValue<>(key, inputMsgTuple);
         }
-    }
-
-    @Bean
-    public Function<KStream<String, OrderInputMsg>, KStream<String, OrderInputMsg>> orderProcessor(){
-        return stringOrderInputMsgKStream -> stringOrderInputMsgKStream
-                .peek((key, value) -> log.info("Order input msg received with key: {} and payload: {}", key, value))
-                .transform(() -> new Transformer<String, OrderInputMsg, KeyValue<String, OrderInputMsg>>() {
-                    ProcessorContext context;
-                    @Override
-                    public void init(ProcessorContext context) {
-                        this.context = context;
-                    }
-                    @Override
-                    public KeyValue<String, OrderInputMsg> transform(String key, OrderInputMsg value) {
-                        try{
-                            final OrderInputMsg msg = OrderProcessorService.processOrderMsg(value);
-                            return new KeyValue<>(key, msg);
-                        }catch (Exception e){
-                            System.err.println("===================ERROR: "+e.getMessage());
-                            myRetryTemplate().execute(retryContext ->{
-                                final int retryCount = retryContext.getRetryCount();
-                                log.warn("Retrying the message key: {}",key);
-                                log.warn("Current Retry Count: {}",retryCount);
-                                return new KeyValue(key, OrderProcessorService.processOrderMsg(value));
-                            }, context2 ->{
-                                context.commit();
-                                return new KeyValue(key, null);
-                            });
-                            context.commit();
-                            return new KeyValue<>(key, null);
-                        }
-                    }
-                    @Override
-                    public void close() {}
-                })
-                .peek((key, value) -> log.info("ORDER CREDIT CARD INFO MASKED FOR KEY: {}, VALUE:{}", key, value))
-                .filter((key, value) -> value!=null);
     }
 
     @Bean
